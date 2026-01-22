7d127acd90884b0923db77be064fdd9ca79f59e4
shui180 <741542229@qq.com>
2026-01-22 22:19:18 +0800

    增加了注释与自己尝试编写

--- Untitled-1.c
+++ Untitled-1.c
@@ -7,6 +7,22 @@ int main()
     //send out HelloWorld
     //输出一个世界你好
     printf("HelloWorld");
+    int b = 69
     return 0;
 }
+int main()
+{
+    //输出字符，\n叫做换行
+    printf("%c\n",'蔡徐坤');
+    printf("%c",'@&*蔡徐坤^#^打篮球很帅');
+    return 0;
+}
+int main()
+{
+    //输出字符串,fd……
+    //存到了str中
+    char str[] ="fdhajlfhd";
+    printf("%s\n",str);
+    return 0;
 
+}
